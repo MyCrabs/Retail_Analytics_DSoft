201a4e7870aa133cf7f70420e7aa010b72bc915d
Tran Van Thanh Duy <thanhduy190903@gmail.com>
2025-10-17 11:27:34 +0700

    Apply DeepFace

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-﻿import sys
-sys.path.append("/home/ai-intern/DUY/Retail_Analytics_DSoft/Yolov7_StrongSORT_OSNet")
-from strong_sort.strong_sort import StrongSORT
+﻿from deepface import DeepFace
+import cv2
 
-print("ok")
+img = cv2.imread("input/test_age.png")
+res = DeepFace.analyze(img, actions=["gender", "age"], enforce_detection=False)
+
+print(res[0]["dominant_gender"], res[0]["age"])
