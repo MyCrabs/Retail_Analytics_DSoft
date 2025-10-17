1577ce37ec09ff57907316f4171e928caec94479
Tran Van Thanh Duy <thanhduy190903@gmail.com>
2025-10-17 15:23:21 +0700

    VINO classification

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-﻿from deepface import DeepFace
+﻿from openvino import Core
 import cv2
+import numpy as np
 
-img = cv2.imread("input/test_age.png")
-res = DeepFace.analyze(img, actions=["gender", "age"], enforce_detection=False)
+core = Core()
+model_path = "weight/age-gender-recognition-retail-0013.xml"
+compiled_model = core.compile_model(model_path, 'CPU')
 
-print(res[0]["dominant_gender"], res[0]["age"])
+age_output = compiled_model.output(0)
+gender_output = compiled_model.output(1)
+
+def predict_image(img):
+    img = cv2.imread(img)
+    img = cv2.resize(img, (62,62))
+    img = img.transpose((2, 0, 1))[None, :]
+    img = img.astype(np.float32)
+    
+    res = compiled_model([img])
+    age = float(res[age_output][0][0][0][0] * 100)
+    prob = float(res[gender_output][0][0][0][0])
+    gender = "Female" if prob > 0.5 else "Male"
+
+    return round(age, 1), gender
+
+
+predict_image("input/thduy.jpg")
